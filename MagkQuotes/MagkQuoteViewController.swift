2deeec92ab1171456c1d6dcdf2758fa48736bf0e
Joshua Tate <jmtate@icloud.com>
2015-07-19 15:14:39 -0600

    Base functionality appears to be working; added TODO: Animations

--- MagkQuotes/MagkQuoteViewController.swift
+++ MagkQuotes/MagkQuoteViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class MagkQuoteViewController: UIViewController {
+    
+    private struct MagickSelectors {
+        static let LongPress = Selector("longPress:")
+    }
 
     // MARK:- Properties
     // MARK:- IBOutlets
@@ -20,12 +24,25 @@ class MagkQuoteViewController: UIViewController {
     // MARK: Private Properties
     private let Grey1 = UIColor(red: 242, green: 242, blue: 242)
     
+    private var quoteCollection: QuoteCollection!
+    private var authorQuotePairs: [(author: Author, quote: Quote)]!
+    
     // MARK:- UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         imageView.animationImages = ImageResource.images
         imageView.animationDuration = 0.55
+        
+        let longPressToPauseAnimation = UILongPressGestureRecognizer(
+            target: self,
+            action: MagickSelectors.LongPress
+        )
+        longPressToPauseAnimation.minimumPressDuration = 0.168
+        view.addGestureRecognizer(longPressToPauseAnimation)
+        
+        quoteCollection = QuoteCollection(fileName: "QuotePropertyList")
+        authorQuotePairs = quoteCollection.generateAuthorQuotePairList()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -43,6 +60,38 @@ class MagkQuoteViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK:- Custom Methods
+    // MARK:- Gesture Recognizers
+    func longPress(press: UILongPressGestureRecognizer) {
+        
+        if press.state == UIGestureRecognizerState.Began {
+            // TODO: Animations
+            imageView.hidden = true
+            let authorAndQuote = authorQuotePairs.removeLast()
+            let quoteText = quoteCollection.authorQuoteString(authorAndQuote)
+            quoteLabel.text = quoteText
+            quoteLabel.hidden = false
+        } else if press.state == UIGestureRecognizerState.Ended {
+            quoteLabel.hidden = true
+            quoteLabel.text = nil
+            imageView.hidden = false
+        }
+    }
+    
+    // MARK: Private Methods
+    private func pauseLayer(layer: CALayer) {
+        let pausedTime = layer.convertTime(CACurrentMediaTime(), fromLayer: nil)
+        layer.speed = 0.0
+        layer.timeOffset = pausedTime
+    }
+    
+    private func resumeLayer(layer: CALayer) {
+        let pausedTime = layer.timeOffset
+        layer.speed = 1.0
+        layer.timeOffset = 0.0
+        layer.beginTime = 0.0
+        let timeSincePause = layer.convertTime(CACurrentMediaTime(), fromLayer: nil) - pausedTime
+        layer.beginTime = timeSincePause
+    }
 }
 
